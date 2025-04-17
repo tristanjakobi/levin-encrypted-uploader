9de92de15bb9357cd4a16662d2436f5efc9b7012
tristanjakobi <tristanjakobi08@gmail.com>
2025-04-17 19:22:47 +0200

    chore: update dependencies and fix type casting issues

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  extends: [
+    '@react-native',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react', 'react-hooks'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
